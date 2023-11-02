e17a2e63488b151166a2f660c30ea43d4e5117d1
EquisBrea <142422819+EquisBrea@users.noreply.github.com>
2023-11-02 15:13:33 +0100

    Ejercicio terminado
    Terminé el ejercicio con una clase fuera del Main utilizando la interface Function.
    Además agregué el ingreso de un String por consola que se transforma en el listado de Strings, para que se más fácil de testear.

--- src/ConversorAMayus.java
+++ src/ConversorAMayus.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+
 public class ConversorAMayus {
-    
+    List<String> entrada;
+    List<String> salida;
+
+
+    public static List<String> ConversorAMayus (List<String> listaEntra, Function<String, String> conversion){
+        List<String> resultado= new ArrayList<>();
+        for (String ingreso: listaEntra) {
+            String convertido = conversion.apply(ingreso);
+            resultado.add(convertido);
+        }
+        return resultado;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,45 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+       /*List listaEjemplo= new ArrayList<String>();
+        listaEjemplo.add( "me ");
+        listaEjemplo.add( "eNcANTa ");
+        listaEjemplo.add( "prOgRaMar ");
+        listaEjemplo.add( "eN ");
+        listaEjemplo.add( "jAVa.");
+           */
 
+        Scanner scn = new Scanner(System.in);
+        String textoConsola = scn.nextLine();
+        List listadoIngresado;
+        listadoIngresado = List.of(textoConsola.split(" "));
+        System.out.println(listadoIngresado);
 
+        List listaEjemplo= new ArrayList<String>();
+        listaEjemplo = listadoIngresado;
 
+        for(Object s: listaEjemplo.stream().toList()){
+            System.out.println(s);
+        }
+        Function<String, String> convertidor = texto -> texto.toUpperCase();
+        List<String> listaEnMayusculas = ConversorAMayus.ConversorAMayus(listaEjemplo, convertidor);
+
+        System.out.println(listaEjemplo);
+
+        System.out.println(listaEnMayusculas);
+
+        for(Object texto: listaEjemplo.stream().toList()){
+            System.out.println(texto);
+        }
+        for(String texto: listaEnMayusculas){
+            System.out.println(texto);
+        }
     }
 }
